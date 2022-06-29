d6b3bc7429571ec1036e96d9f08118f83c55f152
Zev Averbach <averbach_zev@cat.com>
2022-06-29 15:47:13 +0200

    added two new benchmarks for one new comparator. changed some variable names and string formatters for greater clarity, added a docstring for the same purpose

--- bench_class.py
+++ bench_class.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 import typing
 import sys
 
+
 def attributes_in_class():
     class Pet:
         legs: int
@@ -11,57 +12,92 @@ def attributes_in_class():
         def __init__(self, legs, noise) -> None:
             self.legs = legs
             self.noise = noise
-        
+
         def __repr__(self):
             return f"<Pet legs={self.legs} noise='{self.noise}'>"
-    
+
     for _ in range(100000):
         dog = Pet(4, "woof")
         str(dog)
+
 
 def attributes_in_class_with_slots():
     class Pet:
         legs: int
         noise: str
-        __slots__ = 'legs', 'noise'
+        __slots__ = "legs", "noise"
 
         def __init__(self, legs, noise) -> None:
             self.legs = legs
             self.noise = noise
-        
+
         def __repr__(self):
             return f"<Pet legs={self.legs} noise='{self.noise}'>"
-    
+
     for _ in range(100000):
         dog = Pet(4, "woof")
         str(dog)
+
+
+def attributes_in_class_with_slots_factory():
+    lb = "\n        "
+
+    def class_factory(*attrs):
+        class_string = f"""
+global Klass
+class Klass:
+    __slots__ = {attrs}
+
+    def __init__(self, {', '.join(attrs)}):
+        {lb.join(f'self.{attr}={attr}' for attr in attrs)}
+
+    def __repr__(self):
+        return (
+            f'<{{self.__class__.__name__}}' 
+            + "{' '.join(f'{attr}={{getattr(self, {attr})}}' for attr in attrs)}"
+        )
+"""
+        exec(class_string)
+        return Klass
+
+    Pet = class_factory("legs", "noise")
+
+    for _ in range(100000):
+        dog = Pet(4, "woof")
+        str(dog)
+
 
 def attributes_in_dataclass():
     @dataclass
     class Pet:
         legs: int
         noise: str
-    
+
     for _ in range(100000):
         dog = Pet(4, "woof")
         str(dog)
 
+
+attributes_in_dataclass_with_slots = None
 if sys.version_info.minor >= 10:
+
     def attributes_in_dataclass_with_slots():
         @dataclass(slots=True)
         class Pet:
             legs: int
             noise: str
-        
+
         for _ in range(100000):
             dog = Pet(4, "woof")
             str(dog)
+
 
 def attributes_in_namedtuple():
     Pet = namedtuple("Pet", "legs noise")
     for _ in range(100000):
         dog = Pet(4, "woof")
         str(dog)
+
 
 def attributes_in_namedtuple_type():
     class Pet(typing.NamedTuple):
@@ -72,19 +108,48 @@ def attributes_in_namedtuple_type():
         dog = Pet(4, "woof")
         str(dog)
 
+
 def attributes_in_dict():
     for _ in range(100000):
         dog = {"legs": 4, "noise": "woof"}
         str(dog)
 
-__benchmarks__ = [ 
-    (attributes_in_dataclass, attributes_in_class, "Class instead of dataclass"),
-    (attributes_in_dataclass, attributes_in_namedtuple, "Namedtuple instead of dataclass"),
-    (attributes_in_namedtuple, attributes_in_class, "class instead of namedtuple"),
-    (attributes_in_namedtuple, attributes_in_namedtuple_type, "namedtuple class instead of namedtuple"),
-    (attributes_in_class, attributes_in_dict, "dict instead of class"),
-    (attributes_in_class, attributes_in_class_with_slots, "class with slots")
-]
-if sys.version_info.minor >= 10:
-    __benchmarks__.append((attributes_in_dataclass, attributes_in_dataclass_with_slots, "dataclass with slots"))
 
+__benchmarks__ = [
+    (attributes_in_dataclass, attributes_in_class, "Class instead of dataclass"),
+    (
+        attributes_in_dataclass,
+        attributes_in_namedtuple,
+        "Namedtuple instead of dataclass",
+    ),
+    (attributes_in_namedtuple, attributes_in_class, "class instead of namedtuple"),
+    (
+        attributes_in_namedtuple,
+        attributes_in_namedtuple_type,
+        "namedtuple class instead of namedtuple",
+    ),
+    (attributes_in_class, attributes_in_dict, "dict instead of class"),
+    (
+        attributes_in_class,
+        attributes_in_class_with_slots,
+        "class with slots instead of class",
+    ),
+    (
+        attributes_in_class_with_slots,
+        attributes_in_class_with_slots_factory,
+        "class with slots factory instead of class with slots",
+    ),
+    (
+        attributes_in_dict,
+        attributes_in_class_with_slots_factory,
+        "class with slots factory instead of dict",
+    ),
+]
+if attributes_in_dataclass_with_slots:
+    __benchmarks__.append(
+        (
+            attributes_in_dataclass,
+            attributes_in_dataclass_with_slots,
+            "dataclass with slots instead of dataclass",
+        )
+    )
--- suite.py
+++ suite.py
@@ -12,33 +12,46 @@ from rich.text import Text
 REPEAT = 5
 TIMES = 5
 
-def format_delta(a: float, b: float, d: float) -> Text:
-    if a < b:
-        if d < 10:
+
+def format_delta(result: float, comparator: float, delta: float) -> Text:
+    """
+    Color the column a shade of green if the result was faster than the comparator,
+    red if it was slower, and format the string appropriately with %, minus symbol, etc.
+    """
+    minus = ""
+    if abs(delta) > 100:
+        formatter = "{0:.5f} ({1}{2:.1f}%)"
+    else:
+        formatter = "{0:.7f} ({1}{2:.1f}%)"
+    if result < comparator:
+        if delta < 10:
             col = "medium_spring_green"
-        elif 10 <= d < 20:  
+        elif 10 <= delta < 20:
             col = "spring_green1"
-        elif 20 <= d < 40:
+        elif 20 <= delta < 40:
             col = "spring_green2"
         else:
             col = "green1"
-        return Text(f"{a:.3f} ({d:.1f}%)", style=col)
+        return Text(formatter.format(result, minus, delta), style=col)
     else:
-        return Text(f"{a:.3f} (-{d:.1f}%)", style="red")
+        minus = "-"
+        return Text(formatter.format(result, minus, delta), style="red")
 
 
 if __name__ == "__main__":
-    table = Table(title=f"Anti-Pattern Benchmark Suite, repeat={REPEAT}, number={TIMES}")
+    table = Table(
+        title=f"Anti-Pattern Benchmark Suite, repeat={REPEAT}, number={TIMES}"
+    )
 
     table.add_column("Benchmark", justify="right", style="cyan", no_wrap=True)
-    table.add_column("Min", width=7)
-    table.add_column("Max", width=7)
-    table.add_column("Mean", width=7)
-    table.add_column("Min (+)", style="blue", width=15)
-    table.add_column("Max (+)", style="blue", width=15)
-    table.add_column("Mean (+)", style="blue", width=15)
+    table.add_column("Min", width=10)
+    table.add_column("Max", width=10)
+    table.add_column("Mean", width=10)
+    table.add_column("Min (+)", style="blue", width=21)
+    table.add_column("Max (+)", style="blue", width=21)
+    table.add_column("Mean (+)", style="blue", width=21)
 
-    profiles_out = pathlib.Path(__file__).parent / 'profiles'
+    profiles_out = pathlib.Path(__file__).parent / "profiles"
     if not profiles_out.exists():
         profiles_out.mkdir()
     n = 0
@@ -46,31 +59,48 @@ if __name__ == "__main__":
     for f in pathlib.Path(__file__).parent.glob("bench_*.py"):
         if len(sys.argv) > 1 and f.stem != f"bench_{sys.argv[1]}":
             continue
-        i = __import__(f.stem, globals(), locals(), )
+        i = __import__(
+            f.stem,
+            globals(),
+            locals(),
+        )
         if hasattr(i, "__benchmarks__"):
             for benchmark in i.__benchmarks__:
                 n += 1
                 func1, func2, desc = benchmark
-                without_result = timeit.repeat(func1, repeat=REPEAT, number=TIMES)
-                with_result = timeit.repeat(func2, repeat=REPEAT, number=TIMES)
+                comparator_result = timeit.repeat(func1, repeat=REPEAT, number=TIMES)
+                result = timeit.repeat(func2, repeat=REPEAT, number=TIMES)
 
-                delta_mean = (abs(fmean(with_result) - fmean(without_result)) / fmean(without_result)) * 100.0
-                delta_min = (abs(min(with_result) - min(without_result)) / min(without_result)) * 100.0
-                delta_max = (abs(max(with_result) - max(without_result)) / max(without_result)) * 100.0
+                delta_mean = (
+                    abs(fmean(result) - fmean(comparator_result))
+                    / fmean(comparator_result)
+                ) * 100.0
+                delta_min = (
+                    abs(min(result) - min(comparator_result)) / min(comparator_result)
+                ) * 100.0
+                delta_max = (
+                    abs(max(result) - max(comparator_result)) / max(comparator_result)
+                ) * 100.0
 
-                fdelta_min = format_delta(min(with_result), min(without_result), delta_min)
-                fdelta_max = format_delta(max(with_result), max(without_result), delta_max)
-                fdelta_mean = format_delta(fmean(with_result), fmean(without_result), delta_mean)
+                fdelta_min = format_delta(
+                    min(result), min(comparator_result), delta_min
+                )
+                fdelta_max = format_delta(
+                    max(result), max(comparator_result), delta_max
+                )
+                fdelta_mean = format_delta(
+                    fmean(result), fmean(comparator_result), delta_mean
+                )
 
                 table.add_row(
-                              desc,
-                              "{:.3f}".format(min(without_result)),
-                              "{:.3f}".format(max(without_result)),
-                              "{:.3f}".format(fmean(without_result)),
-                              fdelta_min,
-                              fdelta_max,
-                              fdelta_mean,
-                              )
+                    desc,
+                    "{:.7f}".format(min(comparator_result)),
+                    "{:.7f}".format(max(comparator_result)),
+                    "{:.7f}".format(fmean(comparator_result)),
+                    fdelta_min,
+                    fdelta_max,
+                    fdelta_mean,
+                )
 
     console = Console(width=150)
     console.print(table)
